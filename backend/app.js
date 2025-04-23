5c3c5fbcf543bb8d213333238f99f0d55c823425
akashpande1886 <72142288+akashpande1886@users.noreply.github.com>
2025-04-23 12:16:28 +0530

    config

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-const bodyparser = require("body-parser");
+// const path = require("path");
+// const bodyparser = require("body-parser");
 const db = require("./db/db");
 const router = require("./Routes/router");
 const PORT = 8000;
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, "frontend")));
-
-// Set up routes or other configurations
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
-});
 
 app.use(express.json());
 app.use(router);
-// app.use(bodyparser.json());
 
 app.get("/", (req, res) => {
   res.send("Server start");
